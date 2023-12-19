0633018d74222939c5654b6328acf33b3673f18b
Sebastian <sjoerz@skyface.de>
2023-12-19 08:36:29 +0100

    Add prod compose

--- src/config/scylla.py
+++ src/config/scylla.py
@@ -3,11 +3,12 @@ from cassandra.auth import PlainTextAuthProvider
 import os
 
 CASSANDRA_PASSWORD = os.environ.get('CASSANDRA_PASSWORD', 'cassandra')
-
+CASSANDRA_USERNAME = os.environ.get('CASSANDRA_USERNAME', 'cassandra')
+CASSANDRA_HOST = os.environ.get('CASSANDRA_HOST', '127.0.0.1')
 
 auth_provider = PlainTextAuthProvider(
-    username='cassandra', password=CASSANDRA_PASSWORD)
-cluster = Cluster(['127.0.0.1'], auth_provider=auth_provider)
+    username=CASSANDRA_USERNAME, password=CASSANDRA_PASSWORD)
+cluster = Cluster([CASSANDRA_HOST], auth_provider=auth_provider)
 
 session = cluster.connect()
 KEYSPACE = 'test'
